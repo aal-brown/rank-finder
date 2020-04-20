dd8d56542f19795ffe9f2cbf3174ee93121551da
aal-brown <adlai.brown@gmail.com>
2020-04-20 12:40:45 -0600

    Implemented Chrome url query successfully.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import './App.css';
+/* global chrome */
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [view, setView] = useState(0);
+  const [url, setURL] = useState("")
+
+  function createRequestURL(url) {
+
+  }
+
+  chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+  },
+  (tabs) => {
+    console.log(tabs[0].url)
+      setURL(tabs[0].url);
+  });
+
+  useEffect(() => {
+    if (u) {
+      setView(1)
+    }
+
+  })
+
   return (
-    <div className="App">
-    </div>
+      <div>
+        {view === 0 && (
+          {url} 
+        )}
+        {view === 1 && (
+          <div>Loading</div>
+        )}
+      </div>
   );
 }
 
