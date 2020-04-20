6feb37053d5fd649da3a366369108979917bbad0
aal-brown <adlai.brown@gmail.com>
2020-04-20 17:13:06 -0600

    API requests successful, styling mostly completed.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 /* global chrome */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import wheel from './load.png'
+import "./App.css"
 
 function App() {
   const [view, setView] = useState(0);
   const [url, setURL] = useState("")
-
-  function createRequestURL(url) {
-
-  }
+  const [userData, setUserData] = useState({userID: "", rank: 0})
+  const [error, setError] = useState("");
 
   chrome.tabs.query({
       active: true,
@@ -17,22 +17,116 @@ function App() {
   (tabs) => {
     console.log(tabs[0].url)
       setURL(tabs[0].url);
+      console.log()
   });
 
-  useEffect(() => {
-    if (u) {
-      setView(1)
+ 
+  function checkProperURL(url) {
+    console.log("inside checkProperURL")
+    if(url.includes("github")) {
+        return "github"
+    } else if (url.includes("stackoverflow")) {
+        return "stackoverflow"
+    }
+    return false;
+  }
+
+  function getUserID(url, siteName) {
+    console.log("inside getUserID")
+    let userID = ""
+
+    if(siteName === "github") {
+      userID = url.replace("https://github.com/","")
+      console.log(userID)
+    } else {
+      userID = (url.replace("https://stackoverflow.com/users/", "").split("/"))[0]
+    }
+    return userID
+  }
+  
+  function getRank(userID, siteName) {
+    if (siteName === "github") {
+
+      const headers = {
+        "Content-Type": "application/json",
+        "githubId": userID
+      }
+
+      axios.post("https://ratings-api.dev.reputationaire.com/api/request-rating", {
+        "Content-Type": "application/json",
+        "githubId": userID
+      })
+      .then((res) => {
+        return axios.get(`https://ratings-api.dev.reputationaire.com/api/result?id=${res.data.message.uuid_github}&access=f62345e8-9378-425b-b122-ecb4a9610a38`)
+      })
+      .then((res) => {
+        setUserData({"userID": userID, "rank": res.data.message.average})
+        setView(3)
+      })
+      .catch((err) => {
+        setView(2)
+        setError(err)
+      })
+    } else if (siteName === "stackoverflow") {
+      axios.post("https://ratings-api.dev.reputationaire.com/api/stackoverflow/request-rating", {
+        "Content-Type": "application/json",
+        "stackoverflowId": userID
+      })
+      .then((res) => {
+        console.log("stackoverflow",res)
+        return axios.get(`https://ratings-api.dev.reputationaire.com/api/stackoverflow/result?id=${res.data.message.uuid_stackoverflow}&access=f62345e8-9378-425b-b122-ecb4a9610a38`)
+      })
+      .then((res) => {
+        setUserData({"userID": userID, "rank": res.data.message.average})
+        setView(3)
+      })
+      .catch((err) => {
+        setView(2)
+        setError(err)
+      })
+    } else {
+      setView(4)
     }
 
-  })
+  }
+  
+
+  useEffect(() => {
+    let siteName = checkProperURL(url)
+    let userID = ""
+    if(siteName === "github" || siteName === "stackoverflow") {
+      userID = getUserID(url, siteName)
+      setUserData({...userData, "userID": userID})
+      setView(1)
+      getRank(userID, siteName)
+    }
+  },[url])
 
   return (
-      <div>
+      <div className="App">
         {view === 0 && (
-          {url} 
+          <div>{url}</div>
         )}
         {view === 1 && (
-          <div>Loading</div>
+          <div className="loading">
+            <img className="loading-img" src={wheel} alt=""/>
+            <h2 className="loadingText"> Crunching 50 million developer profiles...</h2>
+          </div>
+        )}
+         {view === 2 && (
+          <div>
+            <h1>ERROR</h1>
+            <section>{error}</section>
+          </div>
+        )}
+        {view === 3 && (
+          <div className="stats">
+            <span><b>Username:</b> {userData.userID}</span>
+            <span><b>Rank:</b> {Number(userData.rank.toFixed(5))}</span> 
+          </div>
+        )}
+        {view === 4 && (
+          <div>Invalid page</div>
         )}
       </div>
   );
